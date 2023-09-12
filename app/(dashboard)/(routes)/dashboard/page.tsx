279a793986a7c0df3c8ede06abdddd3460a1b749
Hamza-js <mhamzaawanmp1@gmail.com>
2023-09-12 07:01:08 -0700

    first commit

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const DashboardPage = () => {
+  return (
+    <div>
+      {" "}
+      <Button>Click Here</Button>
+    </div>
+  );
+};
+
+export default DashboardPage;
