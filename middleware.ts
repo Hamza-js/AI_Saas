4cc78908762bdeba94074a699115a30a0a677ac9
Hamza-js <mhamzaawanmp1@gmail.com>
2023-09-14 00:58:10 +0500

    new db password

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/dashboard", "/api/webhook"],
+  publicRoutes: ["/", "/api/web"],
 });
 
 export const config = {
